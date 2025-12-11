83ed9e286e5b9819b37a29dc9eecd47aff16eacf
Julian Stephen <julian.stephen@gmail.com>
2025-12-11 10:45:01 -0600

    fixes #4

---
+++ proto-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pip install -r requirements-proto.txt
+cd ..
+git clone git@github.com:cetanu/envoy_data_plane.git
+cd envoy_data_plane
+python build.py
+cd ..
+rm -rf plugins-adapter/src/envoy || true
+cp -r envoy_data_plane/src/envoy_data_plane_pb2/envoy plugins-adapter/src/
+
+#envoy xds folders 
+git clone https://github.com/cncf/xds.git
+rm -rf plugins-adapter/src/xds  plugins-adapter/src/validate plugins-adapter/src/udpa
+cp -rf xds/python/xds xds/python/validate xds/python/udpa plugins-adapter/src/
+
+cd plugins-adapter
+
